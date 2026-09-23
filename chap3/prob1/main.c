26aee2a5a2a18e695860a30b313b57f8ab4e613c
jojunhan <jjhan2004@gmail.com>
2026-09-23 02:37:11 +0000

    chap3_prob1

--- chap3/prob1/main.c
+++ chap3/prob1/main.c
@@ -1,5 +1,27 @@
 #include<stdio.h>
+#include<string.h>
+#include"copy.h" 
+
+char line[MAXLINE];
+char longest[MAXLINE];
+void copy(char from[], char to[]);
+
 int main(){
-	printf("Hello World\n");
+	int len;
+	int max;
+	max = 0;
+
+	while(fgets(line,MAXLINE,stdin) != NULL){
+		len = strlen(line);
+
+		if (len > max){
+			max = len;
+			copy(line, longest);
+		}
+	}
+
+	if(max>0)
+		printf("%s \n", longest);
+
 	return 0;
 }
